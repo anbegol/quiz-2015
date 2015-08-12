b4d4170dd3d34ac7d0f12645479978c32e44a392
anbegol <anbegol@gmail.com>
2015-08-13 00:57:43 +0200

    Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,21 @@
 var models =  require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId){
+	models.Comment.find({
+		where: {
+			id: Number(commentId)
+		}
+	}).then(function(comment){
+		if(comment) {
+			req.comment = comment;
+			next();
+		} else {
+			next(new Error('No existe commentId=' + commentId))
+		}
+	}).catch(function(error){next(error)});
+};
+
 // GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
 	res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
@@ -7,7 +23,6 @@ exports.new = function(req, res) {
 
 // POST /quizes/:quizId/comments
 exports.create = function(req, res) {
-	console.log('*************************HASTA AQUI**************************');
 	var comment = models.Comment.build(
 		{ texto: req.body.comment.texto,
 		  QuizId: req.params.quizId
@@ -26,4 +41,13 @@ exports.create = function(req, res) {
 				.then(function() { res.redirect('/quizes/'+req.params.quizId)})
 			}	// res.redirect: Redirección HTTP a lista de preguntas
 		}).catch(function(error){next(error)});
+};
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+	req.comment.publicado = true;
+	
+	req.comment.save({fields: ["publicado"]})
+	.then(function(){res.redirect('/quizes/'+req.params.quizId);})
+	.catch(function(error){next(error)});
 };
